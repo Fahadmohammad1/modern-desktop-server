eb04dbaf717b5f92f82a1ba96c5dd595eebbb781
Fahad <205fahadmohammad@gmail.com>
2023-07-29 14:09:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxyhrlo.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run(req, res) {
+  try {
+    
+    await client.connect();
+    const productCollection = client.db('productCollection').collection('product')
+  
+    app.get("/products", async (req, res) => {
+        const products = await productCollection.find({}).toArray()
+
+        res.send({ message: "success", status: 200, data: products });
+    })    
+  } finally {
+
+  }
+}
+
+run().catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
